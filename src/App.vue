3eeedab588f5e813fe2b744cb98d04f466146aa4
Francisco leon <22012000fdlq@gmail.com>
2021-08-13 23:40:38 -0400

    terminado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Apellido</th>
+        <th scope="col">RUT</th>
+        <th scope="col">Edad</th>
+        <th scope="col">Fecha de Nacimiento</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(persona, $index) of personas" :key="$index">
+        <td>{{ $index }}</td>
+        <td>{{ persona.nombre }}</td>
+        <td>{{ persona.apellido }}</td>
+        <td>{{ persona.rut }}</td>
+        <td>{{ persona.edad }}</td>
+        <td>{{ persona.fechaDeNacimiento }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'App',
+  data: () => ({
+    personas: [
+      {
+        nombre: 'Francisco',
+        apellido: 'Leon',
+        rut: '5.222.225-9',
+        edad: 21,
+        fechaDeNacimiento: '22-01-2000'
+      },
+      {
+        nombre: 'Jose',
+        apellido: 'Jimenez',
+        rut: '4.555.555.8',
+        edad: 21,
+        fechaDeNacimiento: '28-01-2000'
+      }
+    ]
+  })
+}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
